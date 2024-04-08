c84f367e1a075e8c1b0740f7e8412656636957f0
Loay-zeid <loayze007@gmail.com>
2024-04-08 06:40:36 -0600

    .

--- assist/scripts/script.js
+++ assist/scripts/script.js
@@ -5,7 +5,7 @@ let voices=[];
 let voiceselect =document.querySelector("select");
 window.speechSynthesis.onvoiceschanged =() =>{
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+    speech.voice = voices[0]; 
 
     voices.forEach((voice,i) =>(voiceselect.options[i] = new Option(voice.name,i)));
 };
