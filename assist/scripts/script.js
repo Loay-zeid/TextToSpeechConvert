9699646e3f6c2e687022d2f8713321a3e4c8c06b
Loay-zeid <loayze007@gmail.com>
2024-04-08 06:52:08 -0600

    editing the image path

--- assist/scripts/script.js
+++ assist/scripts/script.js
@@ -1,43 +1,20 @@
-
 let speech = new SpeechSynthesisUtterance();
+let voices = [];
 
-let voices=[];
+let voiceselect = document.querySelector("select");
 
-let voiceselect =document.querySelector("select");
-window.speechSynthesis.onvoiceschanged =() =>{
+window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0]; 
+    speech.voice = voices[0];
 
-    voices.forEach((voice,i) =>(voiceselect.options[i] = new Option(voice.name,i)));
+    voices.forEach((voice, i) => (voiceselect.options[i] = new Option(voice.name, i)));
 };
- 
-voiceselect.addEventListener("change",()=>{
-    speech.voice = voices[voiceselect.value]; 
-})
 
-document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
+voiceselect.addEventListener("change", () => {
+    speech.voice = voices[voiceselect.value];
 });
 
-let speech = new SpeechSynthesisUtterance();
-
-let voices=[];
-
-let voiceselect =document.querySelector("select");
-window.speechSynthesis.onvoiceschanged =() =>{
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0]; 
-
-    voices.forEach((voice,i) =>(voiceselect.options[i] = new Option(voice.name,i)));
-};
- 
-voiceselect.addEventListener("change",()=>{
-    speech.voice = voices[voiceselect.value]; 
-})
-
 document.querySelector("button").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
 });
-
